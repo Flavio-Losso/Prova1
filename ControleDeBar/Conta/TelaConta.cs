12e8fd64a94a0b92aef663b808a9604a6ef19f08
Flavio <flcutrimlosso@gmai.com>
2023-05-03 13:54:05 -0300

    Bugfix

--- ControleDeBar/Conta/TelaConta.cs
+++ ControleDeBar/Conta/TelaConta.cs
@@ -31,7 +31,7 @@ namespace ControleDeBar.Conta
             NewTela();
             String men = "Menu Conta\r\n";
             UpdateTela(men);
-            Console.WriteLine("Digite 1 Para Abrir Conta\n\rDigite 2 para Registrar ou Editar Pedidos\n\rDigite 3 para Fechar conta\n\rDigite 4 para Visualizar total Faturado do dia");
+            Console.WriteLine("Digite 1 Para Abrir Conta\n\rDigite 2 para Registrar ou Editar Pedidos\n\rDigite 3 para Fechar conta\n\rDigite 4 para Visualizar Contas\n\rDigite 5 para Visualizar total Faturado do dia\n\rDigite 6 para Editar produtos em contas");
             op = Console.ReadLine();
             switch (op)
             {
@@ -59,7 +59,107 @@ namespace ControleDeBar.Conta
 
         private void InserirProdutos()
         {
-            throw new NotImplementedException();
+            int m = Visualizar();
+            if (m > 0)
+            {
+                int id=-1;
+                Conta c = null;
+                Console.WriteLine("Informe o ID da conta");
+                while (c == null)
+                {
+                    id = Convert.ToInt32(Console.ReadLine());
+                    if (id != null && id >= 0)
+                    {
+                        c=guardaConta.SelecionarId(id);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Id invalido tente novamente");
+                    }
+                    if(c == null) Console.WriteLine("Id invalido tente novamente");
+                }
+                ArrayList a = c.QuantProduto;
+                while (true)
+                {
+                    int prods = 0;
+                    foreach (Produto p in a)
+                    {
+                        Console.WriteLine($"Produto:{prods}  {p.Nome}  {p.Tipo}  {p.Valor}");
+                        prods++;
+                    }
+                    Console.WriteLine("Digite 1 para remover e 2 para adicionar produtos e 3 para cancelar");
+                    string aux = Console.ReadLine();
+                    if (aux.Equals("1"))
+                    {
+                        Console.WriteLine("Informe o id do produto");
+                        int idprod = Convert.ToInt32(Console.ReadLine());
+                        if (idprod < 0 || idprod > a.Count)
+                        {
+                            Console.WriteLine("ID invalido");
+                        }
+                        else
+                        {
+                            a.RemoveAt(idprod);
+                            Console.WriteLine("Produto removido com sucesso deseja remover mais algum? S/N");
+                            string resposta = Console.ReadLine();
+                            if (resposta.Equals("N", StringComparison.OrdinalIgnoreCase)) break;
+                            else continue;
+                        }
+                    }else if (aux.Equals("2"))
+                        
+                    {
+                        Produto prodnew = null;
+                        bool continua = true;
+                        while (prodnew == null || continua)
+                        {
+                            NewTela();
+                            Console.WriteLine("Informe o ID do Produto : ");
+                            int i = Convert.ToInt32(Console.ReadLine());
+                            prodnew = guardaProduto.SelecionarId(i);
+                            if (prodnew != null && !continua) break;
+                            else if (prodnew == null)
+                            {
+                                Console.WriteLine("Favor Escrever um ID valido");
+                                Console.ReadLine();
+                            }
+                            if (prodnew != null && continua)
+                            {
+                                int j = 0;
+                                while (j <= 0)
+                                {
+                                    Console.WriteLine("Informe a quantidade de desse produto");
+                                    j = Convert.ToInt32(Console.ReadLine());
+                                    if (j <= 0)
+                                    {
+                                        Console.WriteLine("Informe a quantidade de produto não pode ser menor que 0");
+                                    }
+                                    else
+                                    {
+                                        for (int x = 0; x < j; x++)
+                                        {
+                                            a.Add(prodnew);
+                                        }
+                                        Console.WriteLine("Deseja continuar a adicionar produtos? S/N");
+                                        string resposta = Console.ReadLine();
+                                        if (resposta.Equals("N", StringComparison.OrdinalIgnoreCase)) continua = false;
+                                    }
+                                }
+                            }
+                            
+                        }
+                        break;
+                    }
+                    else if (aux.Equals("3"))
+                    {
+                        break;
+                    }
+                }
+                if (id >= 0)
+                {
+                    c.QuantProduto = a;
+                    guardaConta.Editar(id, c);
+                }
+            }
         }
 
         private void Fechar()
@@ -83,21 +183,77 @@ namespace ControleDeBar.Conta
                 Conta cAtualizada = guardaConta.SelecionarId(i);
                 ArrayList produtosMesa = null;
                 ArrayList produtosMesaAtualizado = new ArrayList();
-                bool continua = true;
+                bool continua = false;
                 if (cAtualizada != null)
                 {
                     Mesa mesa = null;
                     Garcom garcom = null;
+                    Produto produto = null;
+                    produtosMesa = cAtualizada.QuantProduto;
                     Console.WriteLine("Informe o novo ID da mesa");
-                    int idmesa= Convert.ToInt32(Console.ReadLine());
+                    int idmesa = Convert.ToInt32(Console.ReadLine());
                     while (mesa == null) {
                         mesa = guardaMesa.SelecionarId(idmesa);
                         if (mesa == null) Console.WriteLine("ID invalido tente novamente");
                     }
                     cAtualizada.Mesa = mesa;
-                    
-                    conta.QuantProduto = produtosMesa;
-                }
+                    int idgarcom;
+                    Console.WriteLine("Informe o novo ID do garcom");
+                    idgarcom = Convert.ToInt32(Console.ReadLine());
+                    while (garcom == null)
+                    {
+                        garcom = guardaGarcom.SelecionarId(idgarcom);
+                        if (garcom == null) Console.WriteLine("ID invalido tente novamente");
+                    }
+                    cAtualizada.Garcom = garcom;
+                    bool pdA = false;
+                    Console.WriteLine("Deseja Atualizar os produtos S/N");
+                    string a = Console.ReadLine();
+                    if (a.Equals("s", StringComparison.OrdinalIgnoreCase)) pdA = true;
+                    if (pdA)
+                    {
+                        while (produto == null)
+                        {
+                            Console.WriteLine("Informe o ID do Produto : ");
+                            int idprod = Convert.ToInt32(Console.ReadLine());
+                            produto = guardaProduto.SelecionarId(i);
+                            if (produto != null && !continua) break;
+                            else if (produto == null)
+                            {
+                                Console.WriteLine("Favor Escrever um ID valido");
+                                Console.ReadLine();
+                            }
+                            if (produto != null && continua)
+                            {
+                                int j = 0;
+                                while (j <= 0)
+                                {
+                                    Console.WriteLine("Informe a quantidade de desse produto");
+                                    j = Convert.ToInt32(Console.ReadLine());
+                                    if (j <= 0)
+                                    {
+                                        Console.WriteLine("Informe a quantidade de produto não pode ser menor que 0");
+                                    }
+                                    else
+                                    {
+                                        for (int x = 0; x < j; x++)
+                                        {
+                                            produtosMesaAtualizado.Add(produto);
+                                        }
+                                        Console.WriteLine("Deseja continuar a adicionar produtos? S/N");
+                                        string aux = Console.ReadLine();
+                                        if (aux.Equals("N", StringComparison.OrdinalIgnoreCase)) continua = false;
+                                    }
+                                }
+                            }
+                            cAtualizada.QuantProduto = produtosMesaAtualizado;
+                        }
+                    }
+                    else
+                    {
+                        cAtualizada.QuantProduto = produtosMesa;
+                    }
+                    }
                 guardaConta.Editar(i, cAtualizada);
             }
         }
@@ -110,10 +266,12 @@ namespace ControleDeBar.Conta
             ArrayList conta = guardaConta.SelecionaTodos();
             if (conta.Count > 0)
             {
-                Console.WriteLine("ID  |  Cor da Mesa  |  Garçom  |  Total  |  Clientes na mesa");
+                Console.WriteLine("ID  |  Cor da Mesa  |  Garçom  |  Total  |  Clientes na mesa  |  status");
                 foreach (Conta c in conta)
                 {
-                    Console.WriteLine($"{c.Id}  |  Mesa: {c.Mesa.Cor}  |  {c.Garcom}  |  {c.Total}  |  {c.Mesa.Clientes}");
+                    String aberta = "Aberta";
+                    if (c.Aberta == false) aberta = "Fechada"; 
+                    Console.WriteLine($"{c.Id}  |  Mesa: {c.Mesa.Cor}  |  {c.Garcom.Nome}  |  R${c.Total}  |  {c.Mesa.Clientes}  |  {aberta}");
                 }
                 i = 1;
             }
